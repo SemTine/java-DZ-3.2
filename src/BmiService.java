815da68e5291203a0f8a349fb61504bcc3f4c4bb
Oleg <logosemi@mail.ru>
2024-05-25 09:32:12 +0500

    commit 3

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,8 @@
 public class BmiService {
-    public double canculate(double weight, double height) {
+    public int imt(double weight, double height) {
 
-        double boydMassIndex;
-        boydMassIndex = weight / (height * height);
-        return boydMassIndex;
+        double index = weight / (height * height);
+        return (int) index;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
         double weight = 98;
         double height = 1.87;
-        double imt = service.canculate(weight, height);
+        int imt = service.imt(weight, height);
 
-        System.out.println("Вашь вес " + weight + "kg");
-        System.out.println("И рост " + height + "m");
-        System.out.println("Эквивалетно " + (int) imt);
+        System.out.println("Вашь вес " + weight + "kg ");
+        System.out.println("И рост " + height + "m ");
+        System.out.println("Эквивалетно " + imt);
 
     }
 
